8a2ab935f12e08073686156cdbcd8f3368c6479a
1unaram <haram620@icloud.com>
2022-03-02 22:20:05 +0900

    세부 내용 제작

--- client/src/Components/ComplainForm.js
+++ client/src/Components/ComplainForm.js
@@ -8,7 +8,11 @@ import style from './styles/ComplainForm.module.css';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Slider from '@mui/material/Slider';
 
 const ComplainForm = () => {
 
@@ -17,13 +21,14 @@ const ComplainForm = () => {
 
     return (
         <div style={{ backgroundColor: lineInfo.color }}>
-            <Container maxWidth="xs" style={{ backgroundColor: lineInfo.color, height: "100vh" }}>
+            <Container maxWidth="xs" style={{ backgroundColor: lineInfo.color, height: "120vh" }}>
                 <Box
                     sx={{
                         display: "flex",
                         flexDirection: "column",
                         alignItems: "center",
                     }}>
+                    
                     <Typography component="h1" variant="h5" className={style.title} sx={{ mt: 5 }}>
                         {lineInfo.line}호선 열차 민원 접수
                     </Typography>
@@ -40,7 +45,33 @@ const ComplainForm = () => {
                         }}
                         style={{ backgroundColor: "white" }}
                     >
-                        열차번호
+                        <FormGroup>
+                            <Box sx={{display: "flex", flexDirection: "column"}}>
+                                열차번호 
+                                <TextField id="train-number" label="Train" variant="outlined" />
+                            </Box>
+                            <Box sx={{display: "flex", flexDirection: "column"}}>
+                                행선지
+                                <TextField id="destination" label="Destination" variant="outlined" />
+                            </Box>
+                            <Box sx={{display: "flex", flexDirection: "column"}}>
+                                전화번호
+                                <TextField id="phone-number" label="Phone Number" variant="outlined" />
+                            </Box>
+                            <Box sx={{display: "flex", flexDirection: "column"}}>
+                                <Box sx={{display: "flex", flexDirection: "column"}}>
+                                    불편사항
+                                    <FormControlLabel control={<Checkbox />} label="마스크" />
+                                    <FormControlLabel control={<Checkbox />} label="잡상인" />
+                                    <FormControlLabel control={<Checkbox />} label="취객" />
+                                    <FormControlLabel control={<Checkbox />} label="오물" />
+                                    <FormControlLabel control={<Checkbox />} label="난동" />
+                                    <FormControlLabel control={<Checkbox />} label="응급환자" />
+                                    <FormControlLabel control={<Checkbox />} label="몰래카메라 의심" />
+                                </Box>
+                                
+                            </Box>
+                        </FormGroup>
                     </Box>
                 </Box>
             </Container>
